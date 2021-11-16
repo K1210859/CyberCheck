a81d158f790ec24f6335748c764f5c2b4aa62def
Pranav Akkaraju <63757975+K1210859@users.noreply.github.com>
2021-11-16 06:26:41 -0600

    Add files via upload

---
+++ CyberCheck/CyberCheck/testing.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+$sname = "localhost";
+$uname = "root";
+$password = "";
+$db_name = "CyberCheck";
+
+$connection = mysqli_connect($sname, $uname, $password, $db_name);
+
+echo "The error is that there is an ... extra curly brace in the code.";
+
+
+?>
